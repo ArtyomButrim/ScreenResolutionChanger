e99a468d6efcf3e27ae09f1184023f954e506d45
Artyom <artyom.butrim.2000@gmail.com>
2025-03-28 21:44:30 +0300

    add changing display settings and reset to default settings

--- ScreenResolutionChanger/ScreenResolutionChanger.cpp
+++ ScreenResolutionChanger/ScreenResolutionChanger.cpp
@@ -2,25 +2,79 @@
 #include <windows.h>
 
 
-void PrintCurrentDisplaySettings()
+void PrintCurrentDisplaySettings(DEVMODEA *deviceMode)
 {
-	DEVMODEA deviceMode = { 0 };
-	deviceMode.dmSize = sizeof(deviceMode);
-
-	if (EnumDisplaySettingsA(NULL, ENUM_CURRENT_SETTINGS, &deviceMode)) {
+	if (EnumDisplaySettingsA(NULL, ENUM_CURRENT_SETTINGS, deviceMode)) {
 		std::cout << "Current display settings:" << std::endl;
-		std::cout << " - color depth : " << deviceMode.dmBitsPerPel << " bit/pix" << std::endl;
-		std::cout << " - width : " << deviceMode.dmPelsWidth << std::endl;
-		std::cout << " - height : " << deviceMode.dmPelsHeight << std::endl;
-		std::cout << " - frequence : " << deviceMode.dmDisplayFrequency << " Hz" << std::endl;
+		std::cout << " - color depth : " << deviceMode->dmBitsPerPel << " bit/pix" << std::endl;
+		std::cout << " - width : " << deviceMode->dmPelsWidth << std::endl;
+		std::cout << " - height : " << deviceMode->dmPelsHeight << std::endl;
+		std::cout << " - frequence : " << deviceMode->dmDisplayFrequency << " Hz" << std::endl;
 	}
 	else {
 		std::cerr << "Ошибка EnumDisplaySettings: " << GetLastError() << std::endl;
 	}
 }
 
+void ChangeCurrentDisplaySettings(int colorDepth, int width, int height, int frequence)
+{
+	DEVMODEA dm = { 0 };
+	dm.dmSize = sizeof(dm);
+	dm.dmBitsPerPel = colorDepth;
+	dm.dmPelsHeight = height;
+	dm.dmPelsWidth = width;
+	dm.dmDisplayFrequency = frequence;
+	dm.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT | DM_DISPLAYFREQUENCY;
+
+	LONG result = ChangeDisplaySettingsA(&dm, CDS_FULLSCREEN);
+
+	if (result != DISP_CHANGE_SUCCESSFUL)
+	{
+		std::cerr << "Ошибка ChangeDisplaySettingsA: " << result << std::endl;
+	}
+}
+
 int main()
 {
-	PrintCurrentDisplaySettings();
-	return 0;
+	DEVMODEA deviceMode = { 0 };
+	deviceMode.dmSize = sizeof(deviceMode);
+	
+	PrintCurrentDisplaySettings(&deviceMode);
+	
+	DEVMODEA deviceModeDefaultSetup = { 0 };
+	deviceModeDefaultSetup.dmSize = sizeof(deviceModeDefaultSetup);
+	deviceModeDefaultSetup = deviceMode;
+
+	int choice = NULL;
+	while (choice != 3)
+	{
+		std::cout << "------------------------" << std::endl;
+		std::cout << "What do want to do?" << std::endl;
+		std::cout << "1. Change display settings." << std::endl;
+		std::cout << "2. Go to default display settings." << std::endl;
+		std::cout << "3. Close application." << std::endl;
+		std::cout << "------------------------" << std::endl;
+		std::cout << "Your choice(input number):" << std::endl;
+		std::cin >> choice;
+
+		if (choice == 1)
+		{
+			int colorDepth, width, height, frequency;
+			std::cout << "Input color depth(current: " << deviceMode.dmBitsPerPel << " bit/pix):" << std::endl;
+			std::cin >> colorDepth;
+			std::cout << "Input width(current: " << deviceMode.dmPelsWidth << "):" << std::endl;
+			std::cin >> width;
+			std::cout << "Input height(current: " << deviceMode.dmPelsHeight << "):" << std::endl;
+			std::cin >> height;
+			std::cout << "Input frequency(current: " << deviceMode.dmDisplayFrequency << " Hz):" << std::endl;
+			std::cin >> frequency;
+		
+			ChangeCurrentDisplaySettings(colorDepth, width, height, frequency);
+		}
+		else if (choice == 2)
+		{
+			ChangeDisplaySettingsA(&deviceModeDefaultSetup, 0);
+		}
+		PrintCurrentDisplaySettings(&deviceMode);
+	}
 }
